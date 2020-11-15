9b13197a7b5b58e6533c23029635e75ca61c52b4
Joachim Breitner <mail@joachim-breitner.de>
2020-11-15 17:24:21 +0100

    Percentages

--- ext-stats/hackage-count.hs
+++ ext-stats/hackage-count.hs
@@ -37,17 +37,26 @@ main = do
   let total = length sets
   let failures = length [ () | Nothing <- sets ]
   let parsed = total - failures
+  let any_in_cabal = length [ () | Just (_, s2) <- sets, not (Set.null s2) ]
   let tally =
         sortOn snd $ Map.toList $ Map.unionsWith mappend $
-        [ Map.fromSet (const (Sum 1,mempty)) s | Just (s,_) <- sets ] ++
-        [ Map.fromSet (const (mempty, Sum 1)) s | Just (_, s) <- sets ]
+        [ Map.fromSet (const (Sum 1,mempty,mempty)) s | Just (s,_) <- sets ] ++
+        [ Map.fromSet (const (mempty, Sum 1,mempty)) s | Just (_, s) <- sets ] ++
+        [ Map.fromSet (const (mempty, mempty, Sum 1)) (s1 `Set.difference` s2) | Just (s1, s2) <- sets, not (Set.null s2) ]
 
-  forM_ tally $ \(e, (Sum n1, Sum n2)) ->
-    printf "%4d %4d %s\n" (n1::Int) (n2::Int) (showOnOffExtension e)
+  forM_ tally $ \(e, (Sum n1, Sum n2, Sum n3)) ->
+    printf "%s %s %s %s\n"
+      (n1 `outOf` parsed) (n2 `outOf` any_in_cabal) (n3 `outOf` any_in_cabal)
+      (showOnOffExtension e)
   printf "--------------------\n"
   printf "%4d packages total\n" total
   printf "%4d packages parsed\n" parsed
+  printf "%4d packages with extensions in cabal file\n" any_in_cabal
 
+
+outOf :: Int -> Int -> String
+outOf x 0 = "---%"
+outOf x y = printf "%2.0f%%" (fromIntegral x / fromIntegral y * 100 :: Double)
 
 findCabalFile :: FilePath -> IO FilePath
 findCabalFile dirPath = do
